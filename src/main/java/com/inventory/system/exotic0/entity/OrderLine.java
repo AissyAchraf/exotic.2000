bcfdad6887dd2650078cfa5757a26c5d42c97e34
AissyAchraf <achrafaissy1@gmail.com>
2024-03-07 16:06:31 +0100

    Definition of the order line entity attributes

--- src/main/java/com/inventory/system/exotic0/entity/OrderLine.java
+++ src/main/java/com/inventory/system/exotic0/entity/OrderLine.java
@@ -1,2 +1,32 @@
-package com.inventory.system.exotic0.entity;public class OrderLine {
+package com.inventory.system.exotic0.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class OrderLine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "productVariantCode", referencedColumnName = "id", insertable = false, updatable = false)
+    private ProductVariant productVariant;
+    private Float unitPrice;
+    @ManyToOne
+    @JoinColumn(name = "productVariantCode", referencedColumnName = "id", insertable = false, updatable = false)
+    private Order order;
+    private int quantity;
+    private Float originalAmount;
+    @Column(columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isGift = Boolean.FALSE;
+    @Column(columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isHasDiscount = Boolean.FALSE;
+    private Float discountPercentage = 0F;
+    private Float toPayAmount;
 }
