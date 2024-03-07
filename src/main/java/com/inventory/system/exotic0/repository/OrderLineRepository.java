7a2bf5700ee1b10f6fb8257b720615f814b9100b
AissyAchraf <achrafaissy1@gmail.com>
2024-03-07 16:21:25 +0100

    Update OrderLineRepository

--- src/main/java/com/inventory/system/exotic0/repository/OrderLineRepository.java
+++ src/main/java/com/inventory/system/exotic0/repository/OrderLineRepository.java
@@ -1,2 +1,9 @@
-package com.inventory.system.exotic0.repository;public interface OrderLineRepository {
+package com.inventory.system.exotic0.repository;
+
+import com.inventory.system.exotic0.entity.OrderLine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
 }
