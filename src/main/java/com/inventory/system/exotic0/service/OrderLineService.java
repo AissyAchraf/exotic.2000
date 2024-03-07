7e1edd309e3b85667dd44eaff5a3b54e8e05d742
AissyAchraf <achrafaissy1@gmail.com>
2024-03-07 16:59:14 +0100

    Create Order Line Service

--- src/main/java/com/inventory/system/exotic0/service/OrderLineService.java
+++ src/main/java/com/inventory/system/exotic0/service/OrderLineService.java
@@ -1,2 +1,14 @@
-package com.inventory.system.exotic0.service;public interface OrderLineService {
+package com.inventory.system.exotic0.service;
+
+import com.inventory.system.exotic0.entity.Order;
+import com.inventory.system.exotic0.entity.OrderLine;
+
+import java.util.List;
+
+public interface OrderLineService {
+
+    OrderLine save(OrderLine orderLine);
+    OrderLine getById(Long id);
+    List<OrderLine> findAllByOrder(Order order);
+    void delete(OrderLine orderLine);
 }
